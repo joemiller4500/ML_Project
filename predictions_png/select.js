d6e29fb2a81ef4475d246cf4d9a43601699b9699
Criselda Dimacali <59384805+CrisDimacali@users.noreply.github.com>
2020-08-14 22:51:27 -0700

    Delete select.js

--- predictions_png/select.js
+++
@@ -1,6 +0,0 @@
-        
-    function setPicture() {
-        var img = document.getElementById("stockdropdown");
-        var value = img.options[img.selectedIndex].value;
-        alert(selectedValue);
-    }
