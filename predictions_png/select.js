c4a82d39b03c5fae21abd838d92aaaa7cb87e31a
Criselda Dimacali <59384805+CrisDimacali@users.noreply.github.com>
2020-08-14 22:50:50 -0700

    Add files via upload

---
+++ predictions_png/select.js
@@ -0,0 +1,6 @@
+        
+    function setPicture() {
+        var img = document.getElementById("stockdropdown");
+        var value = img.options[img.selectedIndex].value;
+        alert(selectedValue);
+    }
